fcc21372be81c3afe7c928298cd585f79a92ac7f
박기현 <kihyun1998@naver.com>
2025-07-04 14:48:50 +0900

    다른 라이선스 추가중

--- lib/src/models/licenses/bsd_license_info.dart
+++ lib/src/models/licenses/bsd_license_info.dart
@@ -1,5 +1,67 @@
 import '../template_license_info.dart';
 
+class BSD4ClauseLicenseInfo extends TemplateLicenseInfo {
+  @override
+  String get licenseId => 'BSD-4-Clause';
+
+  @override
+  List<String> get heuristicKeywords => [
+        'redistribution and use in source and binary forms',
+        'neither the name of the copyright holder nor the names of its contributors may be used',
+      ];
+
+  @override
+  int get priority => 10;
+
+  @override
+  bool matchesHeuristic(String content) {
+    final lowerContent = content.toLowerCase();
+
+    // BSD-3-Clause 특화 로직
+    return lowerContent
+            .contains('redistribution and use in source and binary forms') &&
+        lowerContent.contains(
+            'neither the name of the copyright holder nor the names of its contributors may be used');
+  }
+
+  @override
+  String get licenseText => r'''
+BSD 4-Clause License
+
+Copyright (c) [year], [fullname]
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. All advertising materials mentioning features or use of this software must
+   display the following acknowledgement:
+     This product includes software developed by [project].
+
+4. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDER "AS IS" AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+''';
+}
+
 class BSD3ClauseLicenseInfo extends TemplateLicenseInfo {
   @override
   String get licenseId => 'BSD-3-Clause';
